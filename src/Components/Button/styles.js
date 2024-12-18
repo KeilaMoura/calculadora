9e4d593bfeeca9544a0be0b502ef4f41359e0632
KeilaMoura <eduardakeylla204@gmail.com>
2024-12-18 19:16:49 -0300

    calculadora simples usando react

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid rgb(235, 228, 228);
+    background-color:rgb(234, 140, 213);
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &: hover{
+        opacity: 0.6 ;
+    }
+
+`;
---
+++ src/Components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color:rgb(143, 20, 114);
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    
+
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color:rgb(143, 20, 114);
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        font-size: 24px;
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        padding: 0 10px;
+        color: #FFFFFF;
+
+    }
+`;
